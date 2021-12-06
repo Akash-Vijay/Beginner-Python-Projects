1a717e2a1c6c814c481292b91e2a799b42036bd0
Akash Vijay <akashvijay@ieee.org>
2021-12-06 18:50:48 +0530

    Added Project 1

---
+++ 01_band_generator.py
@@ -0,0 +1,15 @@
+"""
+
+*   File                  :       01_band_generator.py
+*   Description           :       Program to genearate a band name
+*   Creator               :       Akash Vijay
+*   Date                  :       December 4, 2021
+
+"""
+
+print("\n!! Welcome to Band Name Generator !!\n")
+
+city_name = input("What is your city name ? ")
+pet_name = input("What is your pet name ? ")
+
+print("Your Band Name is " + city_name + " " + pet_name)
